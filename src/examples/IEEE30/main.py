d65a7e80b795872d5092ba10465038e2546e3e07
Eduardo Montilva <edumntg@gmail.com>
2023-10-23 12:13:18 -0400

    First commit

---
+++ src/examples/IEEE30/main.py
@@ -0,0 +1,62 @@
+from PowerSystem import *
+from PowerSystemSolver import *
+import pandas as pd
+
+buses_arr = pd.read_excel('IEEE30.xlsx', 'BUS').to_numpy().astype('float64')
+lines_arr = pd.read_excel('IEEE30.xlsx', 'RAMAS').to_numpy().astype('float64')
+gens_arr = pd.read_excel('IEEE30.xlsx', 'GEN').to_numpy().astype('float64')
+
+# Preprocess
+buses = dict()
+lines = dict()
+gens = dict()
+
+for i, row in enumerate(buses_arr):
+   buses[i]= list(row)
+
+for i, row in enumerate(lines_arr):
+   lines[i] = list(row)
+
+for i, row in enumerate(gens_arr):
+   gens[i] = list(row)
+
+data = {
+    'buses': buses,
+    'lines': lines,
+    'generators': gens,
+    'base': 100 # mva
+}
+
+system = PowerSystem(data)
+# for line in system.lines:
+#    print(str(line))
+
+# Create solver
+solver = PowerSystemSolver_gekko(system)
+
+# Solve
+solver.opf()
+bus, gens, lines = solver.opf_results()
+print("Optimization problem solved. Press enter to continue...")
+input()
+print(bus)
+input()
+print(gens)
+input()
+print(lines)
+# for i in range(system.n_buses):
+#    print(f"V[{i}] = {solver.V[i].value}, theta[{i}] = {solver.theta[i].value}")
+
+# for i in range(system.n_gens):
+#    print(f"Pgen[{i}] = {solver.Pgen[i].value}")
+#    print(f"Qgen[{i}] = {solver.Qgen[i].value}")
+
+# for i in range(system.n_buses):
+#    for j in range(system.n_buses):
+#       print(f"Pflow[{i},{j}] = {solver.Pflow[i,j].value}")
+#       print(f"Qflow[{i},{j}] = {solver.Qflow[i,j].value}")
+
+# for i in range(system.n_lines):
+#    print(f"Line {i} active = ({solver.l[i].value})")
+
+# print('Objective: ' + str(solver.m.options.objfcnval))
---
+++ src/powersys/PowerSystem.py
@@ -0,0 +1,90 @@
+import numpy as np
+from Bus import *
+from Line import *
+from Generator import *
+
+class PowerSystem(object):
+
+    def __init__(self, data: dict):
+        self.buses_raw = data['buses']
+        self.lines_raw = data['lines']
+        self.generators_raw = data['generators']
+
+        # Initialize buses, lines and generators
+        self.buses = []
+        for id, data in self.buses_raw.items():
+            bus = Bus(data)
+            self.buses.append(bus)
+
+        self.lines = []
+        for id, data in self.lines_raw.items():
+            line = Line([id] + data)
+            self.lines.append(line)
+
+        self.generators = []
+        for id, data in self.generators_raw.items():
+            gen = Generator([id] + data)
+            self.generators.append(gen)
+
+        self.Ybus = None
+        self.N = len(self.buses)
+
+        self.n_buses = len(self.buses)
+        self.n_lines = len(self.lines)
+        self.n_gens = len(self.generators)
+
+        self.b = None
+        self.g = None
+        self.G = None
+        self.B = None
+
+    def construct_ybus(self) -> np.array:
+        self.Ybus = np.zeros((self.N, self.N), dtype="complex_")
+        self.b = np.zeros((self.N, self.N))
+        self.g = np.zeros((self.N, self.N))
+
+        # Loop through lines
+        for line in self.lines:
+
+            self.Ybus[line.from_bus, line.from_bus] += line.Y() * (1.0/line.a**2)
+            self.Ybus[line.to_bus, line.to_bus] += line.Y() * (1.0/line.a**2)
+
+            self.Ybus[line.from_bus, line.to_bus] -= line.Y() * 1.0/line.a
+            self.Ybus[line.to_bus, line.from_bus] -= line.Y() * 1.0/line.a
+
+            # Add shunts
+            self.Ybus[line.from_bus, line.from_bus] += 1j*line.B
+            self.Ybus[line.to_bus, line.from_bus] += 1j*line.B
+
+            self.b[line.from_bus, line.to_bus] = line.B
+            self.b[line.to_bus, line.from_bus] = line.B
+            
+        self.G = self.Ybus.real
+        self.B = self.Ybus.imag
+
+        return self.Ybus, self.G, self.B, self.g, self.b
+    
+    def get_gen(self, id):
+        for gen in self.generators:
+            if gen.id == id:
+                return gen
+            
+        return None
+    
+    def get_bus(self, id):
+        for bus in self.buses:
+            if bus.id == id:
+                return bus
+        
+        return None
+
+    def get_line(self, id):
+        for line in self.lines:
+            if line.id == id:
+                return line
+            
+        return None
+
+        
+
+    
