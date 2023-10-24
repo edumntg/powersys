ee2784f041c75abaf249256966a7c1b33556f6ca
Eduardo Montilva <edumntg@gmail.com>
2023-10-23 23:44:32 -0400

    Lot of improvements in DataLoader

---
+++ tests/test_functionality.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('../src/powersys')
+
+from PowerSystem import *
+from PowerSystemSolver import *
+from object_collection import *
+from Bus import *
+from Line import *
+from Generator import *
+import pandas as pd
+
+# buses_arr = pd.read_excel('../src/examples/3-bus-system/data/IEEE30.xlsx', 'BUS').to_numpy().astype('float64')
+# lines_arr = pd.read_excel('../src/examples/3-bus-system/data/IEEE30.xlsx', 'RAMAS').to_numpy().astype('float64')
+# gens_arr = pd.read_excel('../src/examples/3-bus-system/data/IEEE30.xlsx', 'GEN').to_numpy().astype('float64')
+
+# # Preprocess
+# buses = dict()
+# lines = dict()
+# gens = dict()
+
+# for i, row in enumerate(buses_arr):
+#    buses[i]= list(row)
+
+# for i, row in enumerate(lines_arr):
+#    lines[i] = list(row)
+
+# for i, row in enumerate(gens_arr):
+#    gens[i] = list(row)
+
+# data = {
+#     'buses': buses,
+#     'lines': lines,
+#     'generators': gens,
+#     'base': 100 # mva
+# }
+# system = PowerSystem(data)
+system = PowerSystem()
+system.load_buses('ieee30_buses.csv')
+system.load_lines('ieee30_lines.csv')
+system.load_gens('ieee30_gens.csv')
+print(str(system))
+solver = PowerSystemSolver(system)
+
+# Solve
+solver.opf()
+bus, gens, lines = solver.opf_results()
+print("Optimization problem solved. Press enter to continue...")
+input()
+print(bus)
+input()
+print(gens)
+input()
+print(lines)
